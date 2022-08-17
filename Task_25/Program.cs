3ef399b415ea209ab2243929483abe7bd7c3eb83
Igor Matsko <igormazsko@mail.ru>
2022-08-17 20:43:22 +0300

    add correct task 25

--- Task_25/Program.cs
+++ Task_25/Program.cs
@@ -1,20 +1,11 @@
 ﻿// Задача 25. Напишите цикл, который нринимает на ввод два числа (А и В) и возводит число А в натуральную степень В.
 using static System.Console;
 Clear();
-
 Write("Введите число А - ");
 int a = int.Parse(ReadLine());
 Write("Введите число В - ");
 int b = int.Parse(ReadLine());
-// int result = Metod(a, b);
 WriteLine ($"Число {a} в степени {b} равно {DigreeOfNumber(a, b)}");
-
-// int n = 1;
-// for(int i = 1; i < b; i++)
-// {
-//    n = n * a;
-// }
-// WriteLine ($"Число {a} в степени {b} равно {n}");
 
 
 int DigreeOfNumber (int a, int b)
@@ -26,4 +17,3 @@ int DigreeOfNumber (int a, int b)
     }
     return n;
 }
-//WriteLine ( Metod );
