cc00465538d4a814235025fa85489d63eb071542
Igor Matsko <igormazsko@mail.ru>
2022-08-17 21:13:33 +0300

    add task 27

---
+++ Task_27/Program.cs
@@ -0,0 +1,20 @@
+﻿// Задача 27. Напишите программу которая принимает на вход число и выдаёт сумму цифр в числе
+using static System.Console;
+Clear();
+Write("Введите число А - ");
+int a = int.Parse(ReadLine());
+WriteLine ($"Сумма чисел числа {a} равна {SumDigitsNamber(a)}");
+
+
+
+
+int SumDigitsNamber(int n)
+{
+    int result = 0;
+    while(n > 0)
+    {
+        result = n%10 + result;
+        n = n / 10;
+    }
+    return result;
+}
